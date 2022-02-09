db3d46889f4057eb68b708756a690d75b15469ea
manoj <manojnagargoje94@gmail.com>
2022-02-09 20:41:36 +0530

    Add:Creating a contact Details

---
+++ AddressBookSystemDay9/src/com/BrigelabzAddressBook/AddressBook.java
@@ -0,0 +1,12 @@
+package com.BrigelabzAddressBook;
+
+
+	import java.util.ArrayList;
+	import java.util.Scanner;
+
+	public class AddressBook {
+	    Scanner scan=new Scanner(System.in);
+	    ArrayList<ContactDetails> list=new ArrayList<>();
+	}
+
+
